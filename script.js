3cc509330edddca8d5b9cf66ad19114f83541ba4
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-06-19 19:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function filterOdd(arr){
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(arr.filter(x => x % 2 === 0))
+		}, 1000)	
+	})
+}
+
+function multiplay(ar){
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(ar.map(x => x*2));
+		}, 2000)
+	})
+}
+
+const div = document.getElementById("output");
+const input = [1, 2, 3, 4];
+(async function() {
+	try{
+		const filtered = await filterOdd(input)
+		div.innerHTML = filtered.join(",");
+
+		const mult = await multiplay(filtered);
+		div.innerHTML = mult.join(",");
+	}catch(e){
+		console.log(e)
+	}
+})()
